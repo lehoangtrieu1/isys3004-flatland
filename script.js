473679815e052a253b6b1ca9ecfb07ccae6599f5
lehoangtrieu1 <127284462+lehoangtrieu1@users.noreply.github.com>
2024-04-08 09:29:33 +0800

    spelling

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// 1. locat the Element(s)
+
+
+
+
+
+
+
+
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
     let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
